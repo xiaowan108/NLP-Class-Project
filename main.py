efa7bbc0538c76ca74dd01aabc4b640bc1385669
xiaowan108 <neilneil108@gmail.com>
2022-10-30 15:26:33 +0800

    crawer

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from encodings import utf_8
-import re
 import numpy
 import requests
+from bs4 import BeautifulSoup
 
 class Datas:
-    
     stopword = []
     keyword = []
-
-    def getUrl(url, method="get"):
-        r = requests
-        if method == "get":
-            r.get(url)
-        elif method == "post":
-            r.post(url)
+    
+    def getUrl(self, url):
+        r = requests.get(url)
         r.encoding = 'utf-8'
-        print(r.text)
+        main = BeautifulSoup(r.text, 'html.parser')
+        print(main.text)
 
 data = Datas()
-#data.getUrl('https://www.ptt.cc/bbs/cookclub/M.1547728079.A.A7D.html')
-
-r = requests.get('https://www.ptt.cc/bbs/cookclub/M.1547728079.A.A7D.html')
-r.encoding = 'utf-8'
-print(r)
+data.getUrl('https://www.ptt.cc/bbs/cookclub/M.1547728079.A.A7D.html')
